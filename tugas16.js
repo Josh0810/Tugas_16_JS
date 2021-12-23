f78335126c4984ffae072b2361eb764c28beda53
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2021-12-23 22:25:38 +0700

    Add files via upload

---
+++ tugas16.js
@@ -0,0 +1,7 @@
+function MinimalMaximum() {
+    var minimal = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    var maksimal = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+    console.log('Nilai Minimal : ',minimal);
+    console.log('Nilai Maksimal : ',maksimal);
+}
+MinimalMaximum()
